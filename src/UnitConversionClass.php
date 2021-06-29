a1a8372c312c8c2b699d46c1346ea2284f4915ea
hamza <hamza_pisces@live.com>
2021-06-29 19:11:11 +0500

    initial commit

---
+++ src/UnitConversionClass.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Hamza094\UnitConversion;
+
+class UnitConversionClass
+{
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Hamza094\UnitConversion\Tests;
 
 use PHPUnit\Framework\TestCase;
 
