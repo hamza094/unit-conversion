e38c1100b32415f69dc9eff7b77c293b5182e7ef
hamza094 <hamza094@users.noreply.github.com>
2021-06-30 10:54:36 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,17 +4,20 @@ namespace Hamza094\UnitConversions;
 
 class Weight
 {
-   private float $kilograms;
+    private float $kilograms;
 
-  public static function fromKilograms(float $kilograms):self{
-  	return new static($kilograms);
-  } 
+    public static function fromKilograms(float $kilograms):self
+    {
+        return new static($kilograms);
+    }
 
-	public function __construct(float $kilograms){
-		$this->kilograms=$kilograms;
-	}
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
 
-	public function toLbs():float{
-		return $this->kilograms* 2.2046;
-	}
+    public function toLbs():float
+    {
+        return $this->kilograms * 2.2046;
+    }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -11,7 +11,7 @@ class WeightTest extends TestCase
     /** @test */
     public function it_can_convert_kilograms_into_lbs()
     {
-        $lbs=Weight::fromKilograms(100)->toLbs();
-        $this->assertEquals(220.46,$lbs);
+        $lbs = Weight::fromKilograms(100)->toLbs();
+        $this->assertEquals(220.46, $lbs);
     }
 }
